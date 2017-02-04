a04a77fa9b4f34feb8c93f1e56a5919b786eee9c
Manuel Alcocer Jiménez <manuel@alcocer.net>
2017-02-04 18:56:28 +0100

    procedimiento principal

--- P5-ejercicio5/ejercicio5.sql
+++ P5-ejercicio5/ejercicio5.sql
@@ -1,15 +1,31 @@
 create or replace package P5
 as
-    procedure restricciones (nombreTabla dba_tables.table_name%type);
+    procedure restricciones (p_nombreTabla dba_tables.table_name%type);
 end P5;
 /
 
 create or replace package body P5
 as
-    procedure restricciones (nombreTabla dba_tables.table_name%type)
+    function existeTabla (p_nombre dba_tables.table_name%type)
+    return boolean
+    is
+        v_cant number;
+        v_existe boolean := true;
+    begin
+        select count(table_name) into v_cant from dba_tables
+        where lower(table_name) = p_nombre;
+        if v_cant < 1 then
+            v_existe := false;
+        end if;
+        return v_existe;
+    end existeTabla;
+
+    procedure restricciones (p_nombreTabla dba_tables.table_name%type)
     is
     begin
-        NULL;
+        if existeTabla(p_nombreTabla) then
+            null;
+        end if;
     end restricciones;
 end P5;
 /
